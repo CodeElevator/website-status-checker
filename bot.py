b764bf096744f8e1b6a7832ff9ec809d0d295992
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-03-12 15:39:44 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import requests
+
+# basic discord bot setup
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+  print("BOT READY")
+  
+def get_status_code(url: str, headers=None):
+  r = requests.get(url, headers=headers)
+  res = None
+  if r.status_code == 200:
+    res = "Website available"
+  elif r.status_code == 404:
+    res = "Website or page not found"
+  elif r.status_code == 403:
+    res = "Forbidden"
+  elif r.status_code == 400
+    res = "Bad request"
+  else:
+    res = "An error has occured"
+  return res
+
+@bot.command(name="webstatus", aliases=["ws", "status"], description="Show the statuses of websites.")
+async def webstatus(ctx, urls=List[str]):
+  embed = discord.Embed(title="Websites statuses", description="showing website statuses")
+  
